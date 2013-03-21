bc0de45389fc7f71a53965f355ada4be05e36c32
IVAN DARIO TORROLEDO PENA <id.torroledo2887@compufi3.uniandes.edu.co>
2013-03-21 10:05:59 -0500

    version final de la tarea en python

--- tarea5.py
+++ tarea5.py
@@ -25,20 +25,16 @@ for i in range(len(media_mensual)):
   years.append(tiempo[i])
   manchas.append(media_mensual[i])
 
-'''
-plt.plot(years,manchas,'ro')
-plt.ylabel('Potencias vs Frecuencias')
-plt.scatter(years,manchas,'ro')
+plt.plot(years,manchas, 'ro')
+plt.ylabel('Numero de Machas vs Tiempo')
 plt.grid()
 plt.show()
-'''
 
 #Modulo 1: Transformada discreta de fourier
 n=len(manchas)
 fft_x = fft(manchas)/n # FFT Normalized
 timestep = 1.0/12.0
 freq=fftfreq(n, d=timestep)
-
 '''
 plt.plot(freq,np.abs(fft_x))
 plt.ylabel('Potencias vs Frecuencias')
@@ -46,21 +42,19 @@ plt.scatter(freq,np.abs(fft_x))
 plt.grid()
 plt.show()
 '''
-
 #Modulo 2: Espectros de potencias vs f
 f_shifted=np.fft.fftshift(freq)
 fftx_shifted=np.fft.fftshift(fft_x)
 Pot=np.abs(fftx_shifted)*np.abs(fftx_shifted)
 
-'''
 plt.plot(f_shifted,Pot)
 plt.ylabel('Potencias vs Frecuencias')
 plt.scatter(f_shifted,Pot)
 plt.grid()
 plt.show()
-'''
 
 #Modulo 3: Espectros de potencias vs T
+
 T=[]
 new_Pot=[]
 T0=1
@@ -72,19 +66,17 @@ fft_x_half=(2.0)*fft_x[:half_n]
 freq_half=freq[:half_n]
 
 for i in range(1,len(freq_half)):
- Temp=1.0/freq_half[i]
- if Temp>T0:
-  if Temp<Tf:
-   T.append(Temp)
-   new_Pot.append(np.abs(fft_x_half[i])*np.abs(fft_x_half[i]))
-  
-'''
+    Temp=1.0/freq_half[i]
+    if Temp>T0:
+        if Temp<Tf:
+            T.append(Temp)
+            new_Pot.append(np.abs(fft_x_half[i])*np.abs(fft_x_half[i]))
+ 
 plt.plot(T,new_Pot)
-plt.ylabel('Potencias vs T')
+plt.ylabel('Potencias vs Tiempo')
 plt.scatter(T,new_Pot)
 plt.grid()
 plt.show()
-'''
 
 #Modulo 4: Filtro en Frecuencias
 frecuencias=[]
@@ -99,32 +91,71 @@ fft_x[np.abs(freq) < F0] = 0.0
 frecuencias=freq
 Potencias=np.abs(fft_x)*np.abs(fft_x)
 
-'''
 plt.plot(frecuencias,Potencias)
 plt.ylabel('Potencias vs frecuencias filtradas')
 plt.scatter(frecuencias,Potencias)
 plt.grid()
 plt.show()
-'''
 
 #Modulo 5:Antitransformo
 y_real=[]
 t=years
 y_modelo=[]
-clean_f = np.fft.ifft(fft_x) 
+clean_f = np.fft.ifft(fft_x)
 
 for i in range(len(manchas)):
- y_real.append(manchas[i]/np.amax(manchas))
- y_modelo.append(np.real(clean_f[i])/np.amax(np.real(clean_f)))
- 
-'''
+    y_real.append(manchas[i])
+    y_modelo.append(np.real(clean_f[i])/np.amax(np.real(clean_f)) * np.amax(manchas))
+
 plt.plot(t,y_real,'r',t,y_modelo,'g')
 plt.show()
-'''
+
 #Modulo 6:Prediccion
+#buscar el ultimo maximo
 
-
-
-
-
-
+a=0
+ini=0
+for a in range(len(t)):
+    if(t[a]==1995.0):
+        ini=a
+pos=[]
+for i in range(1,100):
+    if(y_modelo[ini-i]<y_modelo[ini-i+1]):
+        pos.append(ini-i)
+  
+amax=t[pos[0]]
+resultado=amax+10.83
+mensaje= "La proxima fecha donde se presentara el mayor numero de manchas solares es:"
+anho= int(resultado)
+mes= abs(resultado)- anho
+#calcular mes
+for i in range(12):
+    if (i/12.0<=mes):
+        if(mes<(i+1)/12.0):
+            if(i==0):
+                mess= "Enero"
+            if(i==1):
+                mess= "Febrero"
+            if(i==2):
+                mess= "Marzo"
+            if(i==3):
+                mess= "Abril"
+            if(i==4):
+                mess= "Mayo"
+            if(i==5):
+                mess= "Junio"
+            if(i==6):
+                mess= "Julio"
+            if(i==7):
+                mess= "Agosto"
+            if(i==8):
+                mess= "Septiembre"
+            if(i==9):
+                mess= "Octubre"
+            if(i==10):
+                mess= "Noviembre"
+            if(i==11):
+                mess= "Diciembre"
+print mensaje
+print mess
+print anho
